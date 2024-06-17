562025f2e3cc4a3383c3e9e62485c408d6138c14
Niewidzialny84 <niewidzialny84@vp.pl>
2024-06-17 19:43:02 +0200

    Initial backend

---
+++ backend/board/src/test/java/niewidzialny84/github/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package niewidzialny84.github.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
